8e435541048e6b78dba7509db0b2864fb4500d9a
Jack Bush <bush.jack@gmail.com>
2023-03-23 21:12:28 +0000

    added projects list (#9)

---
+++ src/content/projects.js
@@ -0,0 +1,11 @@
+export const projects = [
+	{
+		name: 'Inclusive Design Scenarios',
+		description: 'Bite-sized prompts towards better products for everybody.',
+		url_live: 'https://jackbush.github.io/uk-visited-counties/',
+	}, {
+		name: 'Seeing the UK',
+		description: 'Interactive map to track the project of discovering my adoptive home.',
+		url_live: 'https://jackbush.github.io/uk-visited-counties/',
+	}
+]
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,12 +1,24 @@
 import * as React from "react";
+import { Link } from "gatsby";
 import Layout from "../components/layout";
 import { typography } from "../tokens";
+import { projects } from "../content/projects";
 
 export default function Component() {
   return (
     <Layout>
       <main>
         <h1 style={typography.title}>🐳 Projects</h1>
+          <ul>
+            {projects.map((project) => (
+              <li key={ project.name }>
+                <Link to={ project.url_live }>
+                  { project.name }
+                </Link>
+                <p>{ project.description }</p>
+              </li>
+            ))}
+          </ul>
       </main>
     </Layout>
   );
