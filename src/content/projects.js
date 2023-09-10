876d4a9cbefa51677a8c8c3eea7ea6407d05f211
Jack Bush <bush.jack@gmail.com>
2023-09-10 14:55:06 +0100

    linked bharat project #13

--- src/content/projects.js
+++ src/content/projects.js
@@ -2,12 +2,20 @@ export const projects = [
   {
     name: "Inclusive Design Scenarios",
     description: "Bite-sized prompts towards better products for everybody.",
-    url_live: "https://jackbush.github.io/inclusive-design-scenarios/",
+    url: "https://theuxtoolbox/inclusive-design-scenarios/",
+    isWIP: true,
   },
   {
     name: "Seeing the UK",
     description:
-      "Interactive map to track the project of discovering my adoptive home.",
-    url_live: "https://jackbush.github.io/uk-visited-counties/",
+      "Interactive map to track the project of discovering my adoptive homeland.",
+    url: "https://jackbush.github.io/uk-visited-counties/",
+  },
+  {
+    name: "Bharat",
+    description:
+      "Experiments with photos. More fun than posting to the social network everybody hates but nobody can stop using.",
+    url: "https://jackbush.github.io/bharat/",
+    isWIP: true,
   },
 ];
